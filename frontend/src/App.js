25728072a0bfac62f4258e766f94ec396eba950a
niriot-dotcom <72941421+Niriot-dotcom@users.noreply.github.com>
2023-04-04 12:27:17 -0600

    try again with create-react-app.

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import AppRouter from "./components/common/AppRouter";
+import "./index.css";
+import { Footer, Navbar } from "./components";
+import styles from "./style";
+
+function App() {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+
+      <AppRouter />
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
